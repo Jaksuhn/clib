a6673a4e09e5e49a66855346477f265443e08e27
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-12-18 16:07:06 +0000

    .

--- Extensions/AtkUnitBasePtrExtensions.cs
+++ Extensions/AtkUnitBasePtrExtensions.cs
@@ -1,8 +1,5 @@
 ﻿using Dalamud.Game.NativeWrapper;
 using FFXIVClientStructs.FFXIV.Component.GUI;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace clib.Extensions;
 
