16a08dc4a2d6abcf7741a866c75e0bdf61816949
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-12-30 22:35:43 +0000

    enumerable extensions

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace clib.Extensions;
+
+public static class IEnumerableExtensions {
+    public static void ForEach<T>(this IEnumerable<T> _items, Action<T> action) {
+        ArgumentNullException.ThrowIfNull(_items);
+        ArgumentNullException.ThrowIfNull(action);
+
+        foreach (var item in _items) {
+            action(item);
+        }
+    }
+}
