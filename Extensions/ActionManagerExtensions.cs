e936ebfce1c5e4b6bf5b998c47e6d87cdc157564
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-02-10 10:27:47 +0000

    more extensions

--- Extensions/ActionManagerExtensions.cs
+++ Extensions/ActionManagerExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
+using FFXIVClientStructs.FFXIV.Client.Game.MJI;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 namespace clib.Extensions;
@@ -7,9 +9,11 @@ namespace clib.Extensions;
 public static unsafe class ActionManagerExtensions {
     extension(ActionManager) {
         public static bool IsCasting(ActionType actionType, uint actionId) {
-            if (Svc.Objects.LocalPlayer is not { } player) return false;
-            var info = player.Address.As<Character>()->GetCastInfo();
-            return info is not null && info->IsCasting && info->ActionType == (byte)actionType && info->ActionId == actionId;
+            if (Control.GetLocalPlayer() is not null and var player) {
+                var info = player->GetCastInfo();
+                return info is not null && info->IsCasting && info->ActionType == (byte)actionType && info->ActionId == actionId;
+            }
+            return false;
         }
 
         public static bool UseAction(ActionType actionType, uint actionId) {
@@ -23,5 +27,29 @@ public static unsafe class ActionManagerExtensions {
         }
 
         public static bool Teleport(uint aetheryteId, byte subIndex = 0) => UIState.Instance()->Telepo.Teleport(aetheryteId, subIndex);
+
+        public static bool Sprint() {
+            const uint SprintId = 4;
+            const ushort SprintStatus = 50;
+            const uint StellarSprintId = 43357;
+            const ushort StellarSprintStatus = 4398;
+            const uint IslandSprintId = 31314;
+
+            foreach (var s in Control.GetLocalPlayer()->StatusManager.Status) {
+                if (s.StatusId is StellarSprintStatus && s.RemainingTime < 5)
+                    return ActionManager.Instance()->UseAction(ActionType.Action, StellarSprintId);
+                if (s.StatusId is SprintStatus) {
+                    if (MJIManager.Instance()->IsPlayerInSanctuary && s.RemainingTime < 5)
+                        return ActionManager.Instance()->UseAction(ActionType.Action, IslandSprintId);
+                }
+            }
+
+            if (GameMain.Instance()->CurrentTerritoryIntendedUseId is FFXIVClientStructs.FFXIV.Client.Enums.TerritoryIntendedUse.CosmicExploration)
+                return ActionManager.Instance()->UseAction(ActionType.Action, StellarSprintId);
+            else if (MJIManager.Instance()->IsPlayerInSanctuary)
+                return ActionManager.Instance()->UseAction(ActionType.Action, IslandSprintId);
+            else
+                return ActionManager.Instance()->UseAction(ActionType.GeneralAction, SprintId);
+        }
     }
 }
--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -1,12 +1,30 @@
 ﻿namespace clib.Extensions;
 
 public static class IEnumerableExtensions {
-    public static void ForEach<T>(this IEnumerable<T> _items, Action<T> action) {
-        ArgumentNullException.ThrowIfNull(_items);
+    public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action) {
+        ArgumentNullException.ThrowIfNull(source);
         ArgumentNullException.ThrowIfNull(action);
 
-        foreach (var item in _items) {
+        foreach (var item in source) {
             action(item);
         }
+    }
+
+    public static bool None<TSource>(this IEnumerable<TSource> source) {
+        ArgumentNullException.ThrowIfNull(source);
+        if (source is ICollection<TSource> collection)
+            return collection.Count is 0;
+        using var e = source.GetEnumerator();
+        return e.MoveNext();
+    }
+
+    public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(predicate);
+
+        foreach (var item in source) {
+            if (predicate(item)) return false;
+        }
+        return true;
     }
 }
