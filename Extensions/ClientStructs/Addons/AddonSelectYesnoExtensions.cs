f185f2aab3281dd02885301750a9fcf225820df4
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-03-13 09:02:51 +0000

    no

--- Extensions/ClientStructs/Addons/AddonSelectYesnoExtensions.cs
+++ Extensions/ClientStructs/Addons/AddonSelectYesnoExtensions.cs
@@ -13,5 +13,14 @@ public static unsafe class AddonSelectYesnoExtensions {
                 addon->ReceiveEvent(AtkEventType.ButtonClick, 0, &evt, &data);
             }
         }
+
+        public static void No() {
+            var addon = RaptureAtkUnitManager.Instance()->GetAddonByName("SelectYesno");
+            if (addon != null && addon->IsReady) {
+                var evt = new AtkEvent() { Listener = &addon->AtkEventListener, Target = &AtkStage.Instance()->AtkEventTarget };
+                var data = new AtkEventData();
+                addon->ReceiveEvent(AtkEventType.ButtonClick, 1, &evt, &data);
+            }
+        }
     }
 }
