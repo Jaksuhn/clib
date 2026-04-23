41469dd50cbf7cbf7d058bc12429dee03ee77feb
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-04-23 16:21:56 +0100

    add status extensions

---
+++ Extensions/Dalamud/StatusListExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Dalamud.Game.ClientState.Statuses;
+
+namespace clib.Extensions;
+
+public static class StatusListExtensions {
+    private static readonly uint[] TwistOfFateStatusIDs = [1288, 1289];
+
+    extension(StatusList list) {
+        public bool HasTwistOfFate() => list.Any(s => TwistOfFateStatusIDs.Contains(s.StatusId));
+    }
+}
