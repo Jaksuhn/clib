a25bce4643b684633eb599019cffacc71bc4bb5c
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-06-10 11:48:33 +0100

    Create IObjectTableExtensions.cs

---
+++ Extensions/Dalamud/IObjectTableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Plugin.Services;
+
+namespace clib.Extensions;
+
+public static class IObjectTableExtensions {
+    public static bool TryGetByGameObjectId(this IObjectTable objs, ulong gameObjectId, out IGameObject obj) {
+        if (objs.FirstOrDefault(o => o.GameObjectId == gameObjectId) is { } match) {
+            obj = match;
+            return true;
+        }
+        obj = default!;
+        return false;
+    }
+
+    public static unsafe bool TryGetPlayerByContentId(this IObjectTable objs, ulong contentId, out IGameObject obj) {
+        if (objs.OfType<IPlayerCharacter>().FirstOrDefault(o => o.Character->ContentId == contentId) is { } match) {
+            obj = match;
+            return true;
+        }
+        obj = default!;
+        return false;
+    }
+}
