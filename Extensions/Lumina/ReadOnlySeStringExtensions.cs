b1301b1ce9694771db350cb889dd82faca8b9a9e
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-06-10 10:12:45 +0100

    misc

--- Extensions/Lumina/ReadOnlySeStringExtensions.cs
+++ Extensions/Lumina/ReadOnlySeStringExtensions.cs
@@ -8,15 +8,19 @@ public static class ReadOnlySeStringExtensions {
     public static bool ContainsText(this ReadOnlySeString seString, string needle) {
         if (seString.IsEmpty) return false;
         var bytes = new ReadOnlySpan<byte>(Encoding.UTF8.GetBytes(needle));
-        return SeStringExtensions.ContainsText(seString, bytes);
+        return seString.ContainsText(bytes);
+    }
+
+    public static bool ContainsText(this ReadOnlySeString seString, ReadOnlySeString needle) {
+        if (seString.IsEmpty || needle.IsEmpty) return false;
+        return seString.ContainsText(needle.AsSpan());
     }
 
     public static bool ContainsAny(this ReadOnlySeString seString, IEnumerable<string> needles) {
         if (seString.IsEmpty) return false;
         foreach (var n in needles) {
             var bytes = new ReadOnlySpan<byte>(Encoding.UTF8.GetBytes(n));
-            if (SeStringExtensions.ContainsText(seString, bytes))
-                return true;
+            return seString.ContainsText(bytes);
         }
         return false;
     }
---
+++ Extensions/Lumina/SatisfactionNpcExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Lumina.Excel;
+using Lumina.Excel.Sheets;
+
+namespace clib.Extensions;
+
+public static class SatisfactionNpcExtensions {
+    extension(SatisfactionNpc row) {
+        public RowRef<Achievement> Achievement
+            => new(row.ExcelPage.Module, Achievement.FirstOrNull(r => r is { AchievementCategory.RowId: 22, AchievementTarget.RowId: 35, Data: [{ RowId: 150 }, ..] } && r.Description.ContainsText(row.Npc.Value.Singular))?.RowId ?? new());
+    }
+}
