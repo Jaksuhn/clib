b00858af0a7e63bf7c4183764689fded7747232b
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-06-15 12:08:50 +0100

    moron

--- Extensions/Dalamud/IGameGuiExtensions.cs
+++ Extensions/Dalamud/IGameGuiExtensions.cs
@@ -1,14 +1,17 @@
 ﻿using clib.Services;
+using Dalamud.Plugin.Services;
 
 namespace clib.Extensions;
 
 public static class IGameGuiExtensions {
-    public static unsafe bool TryGetAddon<T>(string name, out T* AddonPtr) where T : unmanaged {
-        if (Svc.GameGui.GetAddonByName(name) is { Address: var addr }) {
-            AddonPtr = (T*)addr;
-            return true;
+    extension(IGameGui gui) {
+        public unsafe bool TryGetAddon<T>(string name, out T* AddonPtr) where T : unmanaged {
+            if (Svc.GameGui.GetAddonByName(name) is { Address: var addr }) {
+                AddonPtr = (T*)addr;
+                return true;
+            }
+            AddonPtr = null;
+            return false;
         }
-        AddonPtr = null;
-        return false;
     }
 }
