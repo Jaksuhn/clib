6d03878bde43ff29f4d46eabb13bbe8f3cfbf1ce
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-06-16 22:29:11 +0100

    atk extensions

---
+++ Extensions/ClientStructs/Atk/AtkComponentButtonExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace clib.Extensions;
+
+public static unsafe class AtkComponentButtonExtensions {
+    extension(scoped ref AtkComponentButton target) {
+        public void Click() {
+            fixed (AtkComponentButton* ptr = &target) {
+                if (ptr == null) return;
+
+                var ownerNode = ptr->OwnerNode;
+                var evt = ownerNode->AtkResNode.AtkEventManager.Event;
+                ownerNode->OwnerAddon->ReceiveEvent(evt->State.EventType, (int)evt->Param, evt);
+            }
+        }
+    }
+}
