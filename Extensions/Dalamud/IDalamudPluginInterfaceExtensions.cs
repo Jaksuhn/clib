f0089bea054d99e36e983e23a3b25ca210fdbe5c
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-06-26 23:03:05 +0100

    Update IDalamudPluginInterfaceExtensions.cs

--- Extensions/Dalamud/IDalamudPluginInterfaceExtensions.cs
+++ Extensions/Dalamud/IDalamudPluginInterfaceExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Dalamud.Plugin;
+﻿using clib.Services;
+using Dalamud.Plugin;
+using System.IO;
 using System.Reflection;
 
 namespace clib.Extensions;
@@ -12,21 +14,25 @@ public static class IDalamudPluginInterfaceExtensions {
         internal Lazy<uint> CsVersion => new(() => (uint?)typeof(FFXIVClientStructs.ThisAssembly).Assembly.GetName().Version?.Build ?? 0U);
 
         public bool IsPluginLoaded(string internalName) => pi.InstalledPlugins.Any(p => p.InternalName == internalName && p.IsLoaded);
-    }
 
-    public static object? GetService(this IDalamudPluginInterface pi, string serviceName)
-        => pi.GetType().Assembly.GetType("Dalamud.Service`1", true)?
-        .MakeGenericType(pi.GetType().Assembly.GetType(serviceName, true)!)
-        .GetMethod("Get")?.Invoke(null, BindingFlags.Default, null, [], null);
+        public void InitCustomClientStructs() {
+            FFXIVClientStructs.Interop.Generated.Addresses.Register();
+            InteropGenerator.Runtime.Resolver.GetInstance.Setup(Svc.SigScanner.SearchBase, Svc.Data.GameData.Repositories["ffxiv"].Version, new(Path.Join(pi.ConfigDirectory.FullName, "SigCache.json")));
+            InteropGenerator.Runtime.Resolver.GetInstance.Resolve();
+        }
 
-    public static void ToggleDtr(this IDalamudPluginInterface pi) {
-        var config = pi.GetService("Dalamud.Configuration.Internal.DalamudConfiguration");
-        if (config?.GetFieldOrProperty<List<string>>("DtrIgnore") is { } ignoreList) {
-            if (!ignoreList.Contains(pi.InternalName))
-                ignoreList.Remove(pi.InternalName);
-            else
-                ignoreList.Add(pi.InternalName);
-            config.CallMethod("QueueSave", []);
+        public object? GetService(string serviceName)
+            => pi.GetType().Assembly.GetType("Dalamud.Service`1", true)?.MakeGenericType(pi.GetType().Assembly.GetType(serviceName, true)!).GetMethod("Get")?.Invoke(null, BindingFlags.Default, null, [], null);
+
+        public void ToggleDtr() {
+            var config = pi.GetService("Dalamud.Configuration.Internal.DalamudConfiguration");
+            if (config?.GetFieldOrProperty<List<string>>("DtrIgnore") is { } ignoreList) {
+                if (!ignoreList.Contains(pi.InternalName))
+                    ignoreList.Remove(pi.InternalName);
+                else
+                    ignoreList.Add(pi.InternalName);
+                config.CallMethod("QueueSave", []);
+            }
         }
     }
 }
