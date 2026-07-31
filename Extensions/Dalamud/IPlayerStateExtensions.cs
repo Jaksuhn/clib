91bfcac6fa26a02f036c19054e1483b4d3d8e432
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-07-31 19:14:28 +0100

    Create IPlayerStateExtensions.cs

---
+++ Extensions/Dalamud/IPlayerStateExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using Lumina.Excel;
+using Lumina.Excel.Sheets;
+using IntendedUse = FFXIVClientStructs.FFXIV.Client.Enums.TerritoryIntendedUse;
+
+namespace clib.Extensions;
+
+public static class IPlayerStateExtensions {
+    extension(IPlayerState ps) {
+        public unsafe RowRef<TerritoryType> Territory
+            => ps.IsLoaded && GameMain.Instance()->TerritoryLoadState is 2 ? TerritoryType.GetRowRef(GameMain.Instance()->CurrentTerritoryTypeId) : default;
+        public unsafe IntendedUse TerritoryIntendedUse
+            => ps.IsLoaded && GameMain.Instance()->TerritoryLoadState is 2 ? GameMain.Instance()->CurrentTerritoryIntendedUseId : unchecked((IntendedUse)(-1));
+        public bool IsInSoloDuty
+            => ps.IsLoaded && get_Territory(ps).IsValid && get_Territory(ps).ValueNullable?.TerritoryIntendedUse.ValueNullable?.StructsEnum is IntendedUse.SoloDuty;
+
+        public unsafe bool IsBuddyInStable => ps.IsLoaded && PlayerState.Instance()->IsPlayerStateFlagSet(PlayerStateFlag.IsBuddyInStable);
+    }
+}
