a4a7e2b9cb5188b37531cdbf69232f5235d79ae6
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-08-28 18:01:09 +0100

    add color

--- Extensions/Std/FloatExtensions.cs
+++ Extensions/Std/FloatExtensions.cs
@@ -3,5 +3,9 @@
 namespace clib.Extensions;
 
 public static class FloatExtensions {
-    public static float Scaled(this float f) => f * Dalamud.Interface.Utility.ImGuiHelpers.GlobalScale * (Svc.Interface.UiBuilder.DefaultFontSpec.SizePt / 12f);
+    extension(float f) {
+        public float Scaled() => f * Dalamud.Interface.Utility.ImGuiHelpers.GlobalScale * (Svc.Interface.UiBuilder.DefaultFontSpec.SizePt / 12f);
+        public float Clamp01() => Math.Clamp(f, 0f, 1f);
+        public float LerpTo(float other, float fraction) => f + (other - f) * fraction;
+    }
 }
